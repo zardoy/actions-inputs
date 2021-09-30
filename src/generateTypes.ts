24520f0db94b1e2744664500047fa03a43870ef6
Vitaly Turovsky <vital2580@icloud.com>
2021-10-01 02:32:39 +0300

    gg

--- src/generateTypes.ts
+++ src/generateTypes.ts
@@ -1,5 +1,5 @@
-import { Project, StructureKind } from 'ts-morph'
-import { compilerOptions } from 'generated-module/build/ts-morph-utils'
+import { Project, StatementStructures, StructureKind, Writers } from 'ts-morph'
+import { compilerOptions, createJsdoc } from 'generated-module/build/ts-morph-utils'
 import { join } from 'path'
 import { ensureDir } from 'fs-extra'
 import fs from 'fs'
@@ -13,28 +13,31 @@ const defaultOptions = {
     /**
      * @default CWD/action.yml
      */
-    'action-yml-path': undefined,
+    actionYmlPath: undefined as string | undefined,
     /** runtime specific options */
-    runtime: {
-        /**
-         * Set this to `false` to get `undefined` instead of TypeError if required input is missing
-         */
-        // TODO typings on false
-        throwOnMissingRequired: true,
-        /**
-         * Set this to `false` to get `unefined` instead of TypeError if input's type differs
-         */
-        throwOnIncorrectType: true, // todo more options
-        /**
-         * If `true` setting new value to input will throw a TypeError
-         */
-        readOnlyInputs: false,
-    },
+    /**
+     * Set this to `false` to get `undefined` instead of TypeError if required input is missing
+     */
+    throwOnMissingRequired: true,
+    /**
+     * Set this to `false` to get `unefined` instead of TypeError if input's type differs
+     */
+    throwOnIncorrectType: true, // todo more options
+    /**
+     * If `true` setting new value to input will throw a TypeError
+     */
+    readOnlyInputs: false,
 }
 
 export type Options = Partial<typeof defaultOptions>
 
-export const generateTypes = async (options: Options = {}) => {
+export const generateTypes = async ({
+    actionYmlPath = join(process.cwd(), 'action.yml'),
+    case: inputNameCase = 'preserve',
+    readOnlyInputs = false,
+    throwOnIncorrectType = true,
+    throwOnMissingRequired = true,
+}: Options = {}) => {
     const moduleName = '.actions-inputs'
     const modulePath = join(process.cwd(), `node_modules/${moduleName}`)
     await ensureDir(modulePath)
@@ -46,24 +49,82 @@ export const generateTypes = async (options: Options = {}) => {
         },
     })
 
-    const yaml = parseYaml(await fs.promises.readFile(options['action-yml-path'] ?? join(process.cwd(), 'action.yml'), 'utf-8'))
+    interface ActionYaml {
+        inputs: {
+            [name: string]: {
+                description: string
+                default?: string
+                required: boolean
+            }
+        }
+    }
+
+    const yaml = parseYaml(await fs.promises.readFile(actionYmlPath, 'utf-8')) as ActionYaml
+
+    const inputInterface: StatementStructures = {
+        kind: StructureKind.Interface,
+        name: 'Inputs',
+        properties: [],
+        // properties:
+        isExported: true,
+    }
+    const supportedTypes = ['boolean', 'number', 'string'] as const
+
+    const runtimeTypes: Record<string, { runtimeType: keyof typeof supportedTypes; isRequired: boolean }> = {}
+
+    for (const [inputName, { default: defaultValue, description, required }] of Object.entries(yaml.inputs)) {
+        if (/\s/.test(inputName)) {
+            throw new TypeError(`Input with name ${inputName} must not contain any space characters`)
+        }
+        if (!description) console.warn(`Input ${inputName} should have description property!`)
+
+        const inputNameWithCasing = (() => {
+            switch (inputNameCase) {
+                case 'lower':
+                    return inputName.toLowerCase()
+                case 'preserve':
+                    return inputName
+                case 'upper':
+                    return inputName.toUpperCase()
+            }
+        })()
+
+        const inputType =
+            (description && /^\[(.+)\]/.exec(description)?.[1]) ||
+            (defaultValue
+                ? // it won't work if user follows schema (which is not requried)
+                  typeof defaultValue
+                : 'string')
+
+        /** loose arr.includes() */
+        const isArrayType = <T>(array: readonly T[], incomingType: any): incomingType is T => array.includes(incomingType)
+
+        if (!isArrayType(supportedTypes, inputType)) throw new TypeError(`Input ${inputName} has unsupported types are ${inputType}`)
+
+        inputInterface.properties!.push({
+            name: inputNameWithCasing,
+            hasQuestionToken: throwOnMissingRequired ? !required : true,
+            isReadonly: readOnlyInputs,
+            docs: createJsdoc({
+                default: defaultValue,
+                description,
+            }),
+        })
+        runtimeTypes[inputNameWithCasing]
+    }
 
     const source = project.createSourceFile(join(modulePath, 'index.ts'), {
         statements: [
-            {
-                kind: StructureKind.Interface,
-                name: 'Inputs',
-                // properties:
-                isExported: true,
-            },
+            inputInterface,
             {
                 kind: StructureKind.VariableStatement,
                 isExported: true,
                 declarations: [
                     {
-                        name: 'inputs',
+                        name: 'inputTypes',
                         initializer: writer => {
-                            writer.writeLine('5')
+                            Writers.object({})
+                            writer('5')
                         },
                     },
                 ],
--- tests/index.test.ts
+++ tests/index.test.ts
@@ -41,6 +41,23 @@ exports.inputs = 5;
 `)
     expect(indexDTs).toMatchInlineSnapshot(`
 "export interface Inputs {
+    /**
+     * The commit message that will be used to commit the changed files. Check the README for all interpolation options.
+     *
+     * @default auto-update: replace files from source
+     */
+    COMMIT_MESSAGE?: any;
+    /**
+     * [boolean] Run everything except for the copying, removing and commiting functionality.
+     *
+     */
+    DRY_RUN: any;
+    /**
+     * [number] The number of retries.
+     *
+     * @default
+     */
+    RETRIES?: any;
 }
 export declare let inputs: number;
 "
